725d500a6bf8abb0a0f9413209667ba8d7c5ef5c
Vedika Udgir <vedikaudgir@gmail.com>
2025-09-15 15:36:06 +0530

    problem 7 completed

---
+++ Lab1/problem8.py
@@ -0,0 +1,3 @@
+#8. Ask the user for a string containing lowercase letters, uppercase letters, digits or underscores or combination of all. Write a Python program to check whether the string is a valid identifier.
+# Author - Vedika Udgir
+
